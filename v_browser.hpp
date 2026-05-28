e47f3942f3cdd1cbf9ba7d479aba18b87e4c6b75
shrehanrajsingh <shrehanofficial@gmail.com>
2026-05-28 12:36:49 +0530

    adding doxygen style comments

--- v_browser.hpp
+++ v_browser.hpp
@@ -5,6 +5,9 @@
 
 namespace rs
 {
+/**
+ * @brief Qt widget that renders a view from an HTML parse tree.
+ */
 class v_browser : public QWidget
 {
 private:
@@ -12,9 +15,23 @@ private:
   equinox::HTMLParser &tree;
 
 public:
+  /**
+   * @brief Construct a view browser widget.
+   * @param id Instance id.
+   * @param _Tree HTML parser providing the tree to render.
+   * @param parent Optional parent widget.
+   */
   v_browser (int id, equinox::HTMLParser &_Tree, QWidget *parent = nullptr);
+  /**
+   * @brief Destroy the widget.
+   */
   ~v_browser () = default;
 
+  /**
+   * @brief Build a widget subtree from a parsed node.
+   * @param Node Root node for this widget subtree.
+   * @return Newly created widget.
+   */
   QWidget *build_widget (equinox::Node *);
 
 protected:
